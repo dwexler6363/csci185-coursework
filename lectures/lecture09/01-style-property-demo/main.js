c81442eb9cb349bd213f39f5035ae1c2509db3b2
dwexler6363 <dwexler@unca.edu>
2022-09-20 20:50:06 -0400

    hmwk2 file added

---
+++ lectures/lecture09/01-style-property-demo/main.js
@@ -0,0 +1,22 @@
+
+const makeRainbow = () => {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('#section1').style.backgroundColor = 'red';
+};
+
+const makeBlue = () => {
+    // your code here...
+    console.log('Change background to blue');
+};
+
+const makePink = () => {
+    // your code here...
+    console.log('Change background to pink');
+};
+
+const makeOrange = () => {
+    // your code here...
+    console.log('Change background to orange');
+};
+
