af29523083325a259b94af03fa48f7386b563669
dwexler6363 <dwexler@unca.edu>
2022-10-15 22:12:32 -0400

    finished tutorial09

--- tutorials/tutorial08/circles/sketch.js
+++ tutorials/tutorial08/circles/sketch.js
@@ -5,11 +5,31 @@ function setup() {
 
     // fill('red');
     noFill();
-    circle(100, 200, 50);
-    circle(100, 250, 50);
-    circle(100, 300, 50);
-    circle(100, 350, 50);
-    circle(100, 400, 50);
+        // 1. what do we want to repeat? A: draw a circle
+        // 2. How long do we want to repeat? A: 5 times
+        // 3. what will change each time? A: y position by 50
+
+            let x = canvasWidth / 2;
+            let y = canvasHeight / 2;
+            let size = canvasWidth + 200;
+            let fillColor = 'red';
+            while (size >= 0) {
+                if (fillColor == 'teal'){
+                    fillColor = 'blue';
+                } else {
+                    fillColor = 'teal';
+                }
+                    fill (fillColor);
+                    circle (x, y, size)
+                    size -= 10;
+                    x += 3;
+            }
+
+    // circle(100, 200, 50);
+    // circle(100, 250, 50);
+    // circle(100, 300, 50);
+    // circle(100, 350, 50);
+    // circle(100, 400, 50);
 
     drawGrid(canvasWidth, canvasHeight);
 }
---
+++ tutorials/tutorial09/pixel-art/sketch.js
@@ -0,0 +1,95 @@
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight; 
+
+const heart = [
+    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 2, 2, 3, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 1, 1, 1],
+    [1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1],
+    [1, 1, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+];
+
+const frank = [
+    [0, 0, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 2, 2, 2, 2, 2, 0, 0],
+    [0, 0, 2, 2, 2, 2, 2, 0, 0],
+    [0, 0, 2, 1, 2, 1, 2, 0, 0],
+    [0, 0, 2, 2, 2, 2, 2, 0, 0],
+    [0, 2, 3, 3, 3, 3, 3, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 2, 2, 0, 2, 2, 0, 0]
+];
+    
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    drawGrid(canvasWidth, canvasHeight);
+
+ // first 6 rows of frank
+
+let x = 150;
+let y = 150;
+let pixel = 25;
+let i = 0;
+
+    drawPixelArt (frank, 200, 200, 35);
+    drawPixelArt (heart, 540, 250, 22);
+    drawPixelArt (frank, 985, 200, 35);
+}
+
+
+function drawPixelArt(grid, x, y, pixel) {
+    let i = 0;
+    while (i < grid.length) {
+        drawRow(grid[i], x, y, pixel);
+        i++;
+        y += pixel;
+    } 
+}
+
+function drawRow (row, topX, topY, pixelWidth) {
+    // row + (0. 1, 1, 2, 1, 0)
+    // 0 -> transparent
+    // 1 -> hotpink
+    // 2 -> blak
+    // 3 -> white
+
+    let i = 0;
+        while (i < row.length) {
+        if (row[i] == 0) {
+            noFill();
+        } else if (row[i] == 1) {
+            fill('magenta');
+        } else if (row[i] == 2) {
+            fill('navy'); 
+        } else {
+            fill('white');
+        }
+
+        square(topX, topY, pixelWidth)
+        topX += pixelWidth;
+        i++;
+    }     
+}
+
+
+// This is mousepress code is yours, I just liked the feature and wanted to try it. 
+function mousePressed() {
+    const pixelWidth = Math.random()* 10 + 4;
+    const grids = [frank, heart];
+    const grid = grids[Math.floor(Math.random()* 2)];
+    const topX = mouseX - pixelWidth * grid[0].length / 2;
+    const topY = mouseY - pixelWidth * grid.length / 2;
+    drawPixelArt(grid, topX, topY, pixelWidth,)
+}
