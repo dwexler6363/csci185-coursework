bf8e88124a47ede51b541b5a23a7358114d621ad
dwexler6363 <dwexler@unca.edu>
2022-10-21 18:24:21 -0400

    homework 4 progress

--- hw04/landscapes/sketch.js
+++ hw04/landscapes/sketch.js
@@ -2,30 +2,48 @@ const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight; 
 
     // picks a random number between 0 and 1000:
+    const stars = [];
+
+//create the list
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
     let i = 0;
     while (i < 1000) {
-       circle(
-        Math.random()*canvasWidth, 
-        Math.random()*canvasHeight,
-        Math.random()*8);
+        const data = {
+            x:Math.random()*canvasWidth, 
+            y:Math.random()*canvasHeight,
+            w:Math.random()*8, 
+            color: 'white'
+        };
+        
+        stars.push(data);
        i++;
     }
-    
-    function draw() { //?????????
-        clear();
-        framerate(5);
-        let i = 0;
-        while (i < 1000) {
-           circle(
-            Math.random()*canvasWidth, 
-            Math.random()*canvasHeight,
-            Math.random()*8);
-           i++;
-        }
+}  
+
+function draw() { 
+    clear();
+    frameRate(5);
+    let i = 0;
+    while (i < 1000) {
+        const data = stars[i];
+        fill (data.color)
+       circle(data.x, data.y, data.w);
+       i++;
     }
-}   
+    i = 0;
+    while (i < 100) {
+        const data = stars[i];
+        if (data.color == 'black'){
+            data.color = 'white';
+        } else {
+            data.color = 'black';
+        }
+        fill (data.color)
+       circle(data.x, data.y, data.w);
+       i++;
+    }
+}
     // picks a random number between 10 and 500:
    //rando = Math.random() * 490 + 10;
     //console.log(rando);
---
+++ tutorials/tutorial10/demos/drag-object/sketch.js
@@ -0,0 +1,50 @@
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight; 
+
+const data = {x: 100, y: 100, d: 100, color: 'white'}
+const data1 = {x: 200, y: 200, d: 100, color: 'white'}
+
+let activeShape = data;
+
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+}
+
+    // function mouseClicked() {
+        //how do i move the first circle (data) to where
+        // I just clicked the mouse?
+       // data.x = mouseX;
+       // data.y = mouseY;
+
+// always rnning:
+function draw() {
+    clear();
+    fill(data.color);
+    circle(data.x, data.y, data.d);
+    circle(data1.x, data1.y, data1.d);
+}
+
+function mouseDragged() {
+        //detecting which shape to move
+        let distance = dist(data.x, data.y, mouseX, mouseY);
+        console.log (distance, data.x, data.y, mouseX, mouseY);
+        if (distance < data.d/2) { 
+            console.log("first circle"); 
+            activeShape = data; 
+        } else {
+            console.log("second circle");
+            activeShape = data1;
+        }
+
+    // moving thecorrect shape:
+    activeShape.x = mouseX;
+    activeShape.y = mouseY;
+    // let d = dist(data.x, data.y, mouseX, mouseY);
+    //if (d < data.d/2 || data.color == 'teal') {
+    // data.color = 'teal';  
+}
+
+function mouseReleased() {
+    data.color = 'white';
+}
+
